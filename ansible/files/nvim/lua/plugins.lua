5f14d6942de353564fb90e2fcd4a702168384bbb
Nico Bellack <bellack.n@gmail.com>
2022-12-29 22:45:12 +0100

    Add some stuff...

--- ansible/files/nvim/lua/plugins.lua
+++ ansible/files/nvim/lua/plugins.lua
@@ -1,8 +1,15 @@
-local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
 end
+
+local packer_bootstrap = ensure_packer()
 
 return require("packer").startup(function(use)
     -- Packer can manage itself
@@ -10,8 +17,6 @@ return require("packer").startup(function(use)
 
     use "dracula/vim"
     use "scrooloose/nerdtree"
-
-    use "ryanoasis/vim-devicons"
 
     use { "vim-airline/vim-airline", config = function() require"plugincfg.vim-airline" end}
     use "vim-airline/vim-airline-themes"
