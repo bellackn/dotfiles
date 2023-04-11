0d1f47ed6833addb5dd5dae76b36eec0cad221b4
mmmchimps <mmmchimps@posteo.me>
2023-04-11 19:16:08 +0200

    Add deploy script

---
+++ scripts/deploy_dotfiles_macos.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Use this script to deploy the dotfiles in this repository.
+
+CONFIG_PATH="$HOME/.config"
+
+cp -r ../kitty/* "$CONFIG_PATH"/kitty/
+cp -r ../nvim/* "$CONFIG_PATH"/nvim/
+
+cp ../tmux/.tmux.conf "$HOME"/.tmux.conf
+cp ../skhd/.skhdrc "$HOME"/.skhdrc
+cp ../zsh/.zshrc "$HOME"/.zshrc
+cp ../vscode/settings.json "$HOME/Library/Application Support/Code/User/settings.json"
