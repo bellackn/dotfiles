6351b8d0703512f313807b3ff49620561b87a04a
mmmchimps <mmmchimps@posteo.me>
2023-04-12 11:08:07 +0200

    Update vscode settings

--- scripts/deploy_dotfiles_macos.sh
+++ scripts/deploy_dotfiles_macos.sh
@@ -10,4 +10,4 @@ cp -r ../nvim/* "$CONFIG_PATH"/nvim/
 cp ../tmux/.tmux.conf "$HOME"/.tmux.conf
 cp ../skhd/.skhdrc "$HOME"/.skhdrc
 cp ../zsh/.zshrc "$HOME"/.zshrc
-cp ../vscode/settings.json "$HOME/Library/Application Support/Code/User/settings.json"
+cp ../vscode/settings.json "$HOME/Library/Application Support/VSCodium/User/settings.json"
--- scripts/update_dotfiles_macos.sh
+++ scripts/update_dotfiles_macos.sh
@@ -10,4 +10,4 @@ cp -r "$CONFIG_PATH"/nvim/* ../nvim
 cp "$HOME"/.tmux.conf ../tmux
 cp "$HOME"/.skhdrc ../skhd
 cp "$HOME"/.zshrc ../zsh
-cp "$HOME/Library/Application Support/Code/User/settings.json" ../vscode
+cp "$HOME/Library/Application Support/VSCodium/User/settings.json" ../vscode
