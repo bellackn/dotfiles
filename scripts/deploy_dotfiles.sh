0896d26354e6ea705ea0b53b02f67da8834a5f4b
mmmchimps <mmmchimps@posteo.me>
2023-04-13 14:32:04 +0200

    Generalize more

--- scripts/deploy_dotfiles.sh
+++ scripts/deploy_dotfiles.sh
@@ -10,4 +10,11 @@ cp -r ../nvim/* "$CONFIG_PATH"/nvim/
 cp ../tmux/.tmux.conf "$HOME"/.tmux.conf
 cp ../skhd/.skhdrc "$HOME"/.skhdrc
 cp ../zsh/.zshrc "$HOME"/.zshrc
-cp ../vscode/settings.json "$HOME/Library/Application Support/VSCodium/User/settings.json"
+
+if [[ $(uname -o) == "GNU/Linux" ]]; then
+    VSCODE_SETTINGS="$HOME/.config/VSCodium/User/settings.json"
+else
+    VSCODE_SETTINGS="$HOME/Library/Application Support/VSCodium/User/settings.json"
+fi
+
+cp ../vscode/settings.json "$VSCODE_SETTINGS"
--- scripts/update_dotfiles.sh
+++ scripts/update_dotfiles.sh
@@ -10,4 +10,11 @@ cp -r "$CONFIG_PATH"/nvim/* ../nvim
 cp "$HOME"/.tmux.conf ../tmux
 cp "$HOME"/.skhdrc ../skhd
 cp "$HOME"/.zshrc ../zsh
-cp "$HOME/Library/Application Support/VSCodium/User/settings.json" ../vscode
+
+if [[ $(uname -o) == "GNU/Linux" ]]; then
+    VSCODE_SETTINGS="$HOME/.config/VSCodium/User/settings.json"
+else
+    VSCODE_SETTINGS="$HOME/Library/Application Support/VSCodium/User/settings.json"
+fi
+
+cp "$VSCODE_SETTINGS" ../vscode
