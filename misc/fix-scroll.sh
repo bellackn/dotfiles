a8dd54f3ce6d84e16c2c5e0d2e40353fd6fe6a70
Nico Bellack <nico@bellack.dev>
2024-04-22 17:35:36 +0200

    feat: switch desktop BG on both screens

---
+++ misc/fix-scroll.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+synclient VertScrollDelta=-79
+synclient HorizScrollDelta=-79
--- misc/theme-toggle.sh
+++ misc/theme-toggle.sh
@@ -1,30 +1,30 @@
 #!/usr/bin/env bash
 
 # light
-gtk_light="Matcha-light-azul" #"Layan-Light-Solid"
-#xfwm_light="Skeuos-Blue-Light-XFWM" #"Layan-Light-Solid"
+gtk_light="Matcha-light-azul"
 icons_light="candy-icons"
-desktop_bg_light="/usr/share/backgrounds/xfce/illyria-light-maia.jpg"
+desktop_bg_light="/usr/share/backgrounds/xfce/mountains-1412683.jpg"
 
 # dark
-gtk_dark="Matcha-dark-azul" #"Layan-Dark-Solid"
-#xfwm_dark="Skeuos-Blue-Dark-XFWM" #"Layan-Dark-Solid"
+gtk_dark="Matcha-dark-azul"
 icons_dark="candy-icons"
-desktop_bg_dark="/usr/share/backgrounds/manjaro-wallpapers-18.0/default-adapta.jpg"
+desktop_bg_dark="/usr/share/backgrounds/manjaro-wallpapers-18.0/manjaro-cat.jpg"
 
 # current mode determined by gtk theme
 gtk="$(xfconf-query -c xsettings -p /Net/ThemeName)"
 
 if [[ "$gtk" = "$gtk_light" ]]; then
-xfconf-query -c xsettings -p /Net/ThemeName -s "$gtk_dark"
-xfconf-query -c xfwm4 -p /general/theme -s "$xfwm_dark"
-xfconf-query -c xsettings -p /Net/IconThemeName -s "$icons_dark"
-xfconf-query -c xfce4-panel -p /panels/dark-mode -s true
-xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitoreDP/workspace0/last-image -s "$desktop_bg_dark"
+	xfconf-query -c xsettings -p /Net/ThemeName -s "$gtk_dark"
+	xfconf-query -c xfwm4 -p /general/theme -s "$xfwm_dark"
+	xfconf-query -c xsettings -p /Net/IconThemeName -s "$icons_dark"
+	xfconf-query -c xfce4-panel -p /panels/dark-mode -s true
+	xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitoreDP/workspace0/last-image -s "$desktop_bg_dark"
+	xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorDisplayPort-6/workspace0/last-image -s "$desktop_bg_dark"
 else
-xfconf-query -c xsettings -p /Net/ThemeName -s "$gtk_light"
-xfconf-query -c xfwm4 -p /general/theme -s "$xfwm_light"
-xfconf-query -c xsettings -p /Net/IconThemeName -s "$icons_light"
-xfconf-query -c xfce4-panel -p /panels/dark-mode -s false
-xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitoreDP/workspace0/last-image -s "$desktop_bg_light"
+	xfconf-query -c xsettings -p /Net/ThemeName -s "$gtk_light"
+	xfconf-query -c xfwm4 -p /general/theme -s "$xfwm_light"
+	xfconf-query -c xsettings -p /Net/IconThemeName -s "$icons_light"
+	xfconf-query -c xfce4-panel -p /panels/dark-mode -s false
+	xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitoreDP/workspace0/last-image -s "$desktop_bg_light"
+	xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorDisplayPort-6/workspace0/last-image -s "$desktop_bg_light"
 fi
