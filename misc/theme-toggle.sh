ff9aeb78e3aac9f2c352c70ab35f4dd9502db6f8
Nico Bellack <nico@bellack.dev>
2024-08-12 15:25:39 +0200

    fix: theme toggle

--- misc/theme-toggle.sh
+++ misc/theme-toggle.sh
@@ -2,20 +2,25 @@
 
 # light
 gtk_light="Adwaita"
-# xfwm_light="Adwaita"
+xfwm_light="Adwaita"
 icons_light="Papirus-Light"
 desktop_bg_light="/usr/share/backgrounds/xfce/mountains-1412683.jpg"
 alacritty_theme_light="papercolor_light"
 
 # dark
 gtk_dark="Adwaita-dark"
-# xfwm_dark="Adwaita-dark"
+xfwm_dark="Adwaita-dark"
 icons_dark="Papirus-Dark"
 desktop_bg_dark="/usr/share/backgrounds/manjaro-wallpapers-18.0/manjaro-cat.jpg"
 alacritty_theme_dark="challenger_deep"
 
 # current mode determined by gtk theme
 gtk="$(xfconf-query -c xsettings -p /Net/ThemeName)"
+
+# workaround for apps picking up the correct color scheme...
+xfce4-appearance-settings &
+pid=$!
+sleep 0.1
 
 if [[ "$gtk" = "$gtk_light" ]]; then
   xfconf-query -c xsettings -p /Net/ThemeName -s "$gtk_dark"
@@ -34,3 +39,6 @@ else
   xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorDisplayPort-1/workspace0/last-image -s "$desktop_bg_light"
   sed -i -e "s/$alacritty_theme_dark/$alacritty_theme_light/" ~/.config/alacritty/alacritty.toml
 fi
+
+kill $pid
+exit 0
